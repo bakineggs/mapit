0fee0d9b8ec36ef3ea0486573858a185f5f1b5e3
Dan Barry <dan@chaptercommunications.com>
2008-09-19 00:45:03 -0500

    use the workaround mapurl.php

--- getOutput.php
+++ getOutput.php
@@ -2,11 +2,6 @@
 public static function getOutput() {
   $ret = array();
   define("SMDEFAULT", "");
-  define('STREET_ZIP_SMID', 'TuZ');
-  define('ZIP_SMID', 'xOZ');
-  define('STREET_CITY_STATE_SMID', '4h1');
-  define('CITY_STATE_SMID', 'W61');
-  define('STATE_SMID', 'alx');
 
   /* If you leave these blank, the default title and summary will be shown */
   $ret['title'] = SMDEFAULT;
@@ -38,36 +33,7 @@ public static function getOutput() {
   $ret['dict'][3]['key'] = SMDEFAULT;
   $ret['dict'][3]['value'] = SMDEFAULT;
 
-  $address_info = array(
-    'zip' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:postal-code'),
-    'state' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:region'),
-    'city' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:locality'),
-    'street' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:street-address')
-  );
-
-  if ($address_info['zip']) {
-    if ($address_info['street']) {
-      $smid = STREET_ZIP_SMID;
-    }
-    else {
-      $smid = ZIP_SMID;
-    }
-  } else if ($address_info['state']) {
-    if ($address['city']) {
-      if ($address['street']) {
-        $smid = STREET_CITY_STATE_SMID;
-      } else {
-        $smid = CITY_STATE_SMID;
-      }
-    } else {
-      $smid = STATE_SMID;
-    }
-  }
-
-  if (!$smid)
-    return array();
-
-  $img = Data::get('smid:' . $smid . '/media:Thumbnail/@resource');
+  $img = Data::get('smid:1GQ/media:Thumbnail/@resource');
 
   if ($img == 'http://not_real')
     return array();
---
+++ mapurl.php
@@ -0,0 +1,46 @@
+<?php
+header('Content-Type: text/xml');
+
+$doc = DOMDocument::loadHTMLFile($_GET['identifier']);
+
+$parameters = address_parameters($doc) + array(
+  'appid' => 'tCa4.fDV34GcXMnIbz5dWxSZWjSWsMXIQSqiVVBFb55nTKX1RFLqYyuM677Jz2vK'
+);
+
+$joined_parameters = array();
+foreach ($parameters as $key => $value)
+  $joined_parameters[] = urlencode($key) . '=' . urlencode($value);
+
+$url_parameters = '?' . implode('&', $joined_parameters);
+
+readfile('http://local.yahooapis.com/MapsService/V1/mapImage' . $url_parameters);
+
+function address_parameters($doc) {
+  $xpath = new DOMXPath($doc);
+  $addresses = $xpath->evaluate("//*[@class='adr']", $doc);
+  foreach ($addresses as $address) {
+    if ($zip = nodeValue($xpath, $address, 'postal-code')) {
+      $params = array('zip' => $zip);
+      if ($state = nodeValue($xpath, $address, 'region'))
+        $params['state'] = $state;
+      if ($city = nodeValue($xpath, $address, 'locality'))
+        $params['city'] = $city;
+      if ($street = nodeValue($xpath, $address, 'street-address'))
+        $params['street'] = $street;
+      return $params;
+    } else if ($state = nodeValue($xpath, $address, 'region')) {
+      $params = array('state' => $state);
+      if ($city = nodeValue($xpath, $address, 'locality'))
+        $params['city'] = $city;
+      if ($street = nodeValue($xpath, $address, 'street-address'))
+        $params['street'] = $street;
+      return $params;
+    }
+  }
+  return array();
+}
+
+function nodeValue($xpath, $address, $class) {
+  return $xpath->evaluate("//*[@class='" . $class . "']", $address)->item(0)->nodeValue;
+}
+?>
