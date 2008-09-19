f8d549ba31116c50921c764b19aa48b072ebe81a
Dan Barry <dan@chaptercommunications.com>
2008-09-18 19:05:15 -0500

    getOutput finds an image

--- getOutput.php
+++ getOutput.php
@@ -2,6 +2,11 @@
 public static function getOutput() {
   $ret = array();
   define("SMDEFAULT", "");
+  define('STREET_ZIP_SMID', 'TuZ');
+  define('ZIP_SMID', 'xOZ');
+  define('STREET_CITY_STATE_SMID', '4h1');
+  define('CITY_STATE_SMID', 'W61');
+  define('STATE_SMID', 'alx');
 
   /* If you leave these blank, the default title and summary will be shown */
   $ret['title'] = SMDEFAULT;
@@ -33,11 +38,45 @@ public static function getOutput() {
   $ret['dict'][3]['key'] = SMDEFAULT;
   $ret['dict'][3]['value'] = SMDEFAULT;
 
+  $address_info = array(
+    'zip' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:postal-code'),
+    'state' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:region'),
+    'city' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:locality'),
+    'street' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:street-address')
+  );
+
+  if ($address_info['zip']) {
+    if ($address_info['street']) {
+      $smid = STREET_ZIP_SMID;
+    }
+    else {
+      $smid = ZIP_SMID;
+    }
+  } else if ($address_info['state']) {
+    if ($address['city']) {
+      if ($address['street']) {
+        $smid = STREET_CITY_STATE_SMID;
+      } else {
+        $smid = CITY_STATE_SMID;
+      }
+    } else {
+      $smid = STATE_SMID;
+    }
+  }
+
+  if (!$smid)
+    return array();
+
+  $img = Data::get('smid:' . $smid . '/media:Thumbnail/@resource');
+
+  if (!$img)
+    return array();
+
   /* This is for infobar apps
      You can put a subset of HTML in here
      See the docs for more details */
   $ret['infobar']['summary'] = SMDEFAULT;
-  $ret['infobar']['blob'] = SMDEFAULT;
+  $ret['infobar']['blob'] = '<img src="' . htmlentities($img, ENT_QUOTES) . '" />';
 
   return $ret;
 }
