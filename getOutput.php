874987ba2930c66520a43a7484685864ee6ee7a1
Dan Barry <dan@chaptercommunications.com>
2008-09-19 03:25:35 -0500

    address info in infobar summary

--- getOutput.php
+++ getOutput.php
@@ -5,8 +5,16 @@ public static function getOutput() {
   if ($img == 'http://not_real')
     return array();
 
+  $address_parts = array_filter(array(
+    'street' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:street-address'),
+    'city' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:locality'),
+    'state' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:region'),
+    'zip' => Data::get('com.yahoo.uf.hcard/rel:Card/vcard:adr/vcard:postal-code')
+  ));
+
   return array(
     'infobar' => array(
+      'summary' => 'Map of ' . implode(', ', $address_parts),
       'blob' => '<img src="' . htmlentities($img, ENT_QUOTES) . '" />'
     )
   );
